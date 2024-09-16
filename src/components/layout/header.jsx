374d1710a3029d1b6747479f4626b983c80906d4
loannguyen <nguyenloandhnn96@gmail.com>
2024-09-16 11:21:00 +0700

    video 47

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,21 @@
+import "../layout/header.css";
+
+const Header = () => {
+  return (
+    <ul>
+      <li>
+        <a class="active" href="/">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/users">Users</a>
+      </li>
+      <li>
+        <a href="/products">Products</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
