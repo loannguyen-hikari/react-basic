c11131be424c22e2ef1942d22fb2364d432f3279
loannguyen <nguyenloandhnn96@gmail.com>
2024-09-23 11:08:24 +0700

    video 83

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -4,6 +4,22 @@ import { Button, Drawer } from "antd";
 const ViewUserDetail = (props) => {
   const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
 
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  const handleOnChangeFile = (event) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      selectedFile(null);
+      setPreview(null);
+      return;
+    }
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+
   return (
     <Drawer
       width={"40vw"}
@@ -22,9 +38,16 @@ const ViewUserDetail = (props) => {
           <p>Email: {dataDetail.email}</p>
           <br />
           <p>Avatar:</p>
-          <div>
+          <div
+            style={{
+              width: "100px",
+              height: "100px",
+              marginTop: "10px",
+              border: "1px solid #ccc",
+            }}
+          >
             <img
-              style={{ width: "100px", height: "100px" }}
+              style={{ width: "100%", height: "100%", objectFit: "contain" }}
               src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
                 dataDetail.avatar
               }`}
@@ -47,8 +70,29 @@ const ViewUserDetail = (props) => {
             >
               Upload Avatar
             </label>
-            <input type="file" hidden id="btnUpload" />
+            <input
+              type="file"
+              hidden
+              id="btnUpload"
+              onChange={handleOnChangeFile}
+            />
           </div>
+          {preview && (
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                marginTop: "10px",
+                border: "1px solid #ccc",
+              }}
+            >
+              <img
+                style={{ width: "100%", height: "100%", objectFit: "contain" }}
+                src={preview}
+                alt=""
+              />
+            </div>
+          )}
         </>
       ) : (
         <>
