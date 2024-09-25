fbbedaade8fe495295b541eb97c7f99f492e6d68
loannguyen <nguyenloandhnn96@gmail.com>
2024-09-25 19:23:37 +0700

    video 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,81 @@
+import { Input, Button, Form } from "antd";
 
 const RegisterPage = () => {
-  return(
-    <div>Register page</div>
-  )
-}
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      name="basic"
+      onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
+    >
+      <div
+        style={{
+          margin: "50px",
+        }}
+      >
+        <Form.Item
+          label="Full name"
+          name="fullName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your full name!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Phone number"
+          name="phone"
+          rules={[
+            {
+              required: true,
+              message: "Please input your phone number!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <div>
+          <Button type="primary" onClick={() => form.submit()}>
+            Register
+          </Button>
+        </div>
+      </div>
+    </Form>
+  );
+};
 
 export default RegisterPage;
